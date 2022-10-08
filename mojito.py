4ff4e5e1acb031d0857952956c213657f3bbdd61
twixxqz <115325310+twixxqz@users.noreply.github.com>
2022-10-08 18:56:31 +0300

    Add files via upload

---
+++ mojito.py
@@ -0,0 +1,44 @@
+import os
+from time import sleep
+import colorama
+from colorama import Back, Fore, Style
+
+os.system('sudo apt install python3-pip')
+os.system('clear')
+print(Fore.LIGHTGREEN_EX + '________________')
+print('____________n__i')
+print('____________no_in______________enwijm__mjtnn')
+print('_____nwwine_et_mniie________eij_ottttttttim_tn')
+print('_____wjjtttwni__ojtn_______imottjjjjiijjjjttomn')
+print('_____eijjjtttij_mttn_____eo_ttttjjjjijjjjjtttjon')
+print('______eojjjittj__iie_____i_iijjjjtijtijitijjitmt')
+print('________nitjjio___________________oitttjjjjjjtoie')
+print('____________ei_i_owwwwwwwt_ittttti__jiittijjit_t')
+print('____________ei_wotowwwwwwwttottjjjm_ojjjjjtttmje')
+print('____________ei_wwtoowwwwwiiiwooijjm_jjjjjiti_tn')
+print('_____________t_wiwowwwwwiwooowoooto_itttim_tn')
+print('_____________o_oiwwwwwwwwooooowooj_____jon')
+print('_____________w_iiwwwwiiwwtittiiitw__n')
+print('_____________n_jiiwiiiwooijjttttwo_oe')
+print('_____________noowiwowwwwwojjjttwwt_jn')
+print('_____________nj_wwoowwwwoowojjtwwi_ie')
+print('______________t_owoowwooowwijtwwwo_t')
+print('______________o_twjtttiwwtjjttwwwm_w')
+print('______________n_owtjittttjjitowwo__n')
+print('______________no_wwwiiiitjitwwwwj_jn')
+print('_______________t_mwoiittiiiiitoi__o')
+print('_______________no________________on')
+print('________________ni______________in')
+print('___________________ennnneeeenne' + Style.RESET_ALL)
+
+sleep(1)
+
+os.system('sudo apt install wget')
+os.system('sudo wget https://github.com/bottlesdevs/Bottles/releases/download/2021.12.28-treviso/com.usebottles.bottles_2021.12.28-treviso_amd64.deb')
+os.system('sudo apt install ./com.usebottles.bottles_2021.12.28-treviso_amd64.deb')
+os.system('sudo apt install libpython2-stdlib libpython2.7-minimal libpython2.7-stdlib python-pkg-resources python-setuptools python2 python2-minimal python2.7 python2.7-minimal')
+print(Fore.BLACK + Back.LIGHTGREEN_EX + 'Thank you for using mojito as your bottle installer' + Style.RESET_ALL)
+print(Fore.BLACK + Back.GREEN + 'bottles should now appear in your menu!' + Style.RESET_ALL)
+sleep(10)
+
+#Да я использовал кучу команд print просто так хоть мог написать 1 раз и не париться:)
