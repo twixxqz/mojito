cb636835037101fb0355eb334991a7aafcf090ab
twixxqz <115325310+twixxqz@users.noreply.github.com>
2022-10-09 23:15:09 +0300

    Update mojito.py

--- mojito.py
+++ mojito.py
@@ -1,12 +1,14 @@
 import os
-from time import sleep
-import colorama
-from colorama import Back, Fore, Style
 
 os.system('sudo apt install python3-pip')
 os.system('pip install colorama ')
 os.system('sudo pip install colorama')
 os.system('sudo apt install libpython2-stdlib libpython2.7-minimal libpython2.7-stdlib python-pkg-resources python-setuptools python2 python2-minimal python2.7 python2.7-minimal')
+
+from time import sleep
+import colorama
+from colorama import Back, Fore, Style
+
 os.system('clear')
 print(Fore.LIGHTGREEN_EX + '________________')
 print('____________n__i')
@@ -33,12 +35,16 @@ print('_______________t_mwoiittiiiiitoi__o')
 print('_______________no________________on')
 print('________________ni______________in')
 print('___________________ennnneeeenne' + Style.RESET_ALL)
-print('')
-sleep(1)
+print(Fore.LIGHTGREEN_EX)
+sleep(2)
 
+os.system('ls')
+
+print(Style.RESET_ALL)
 os.system('sudo apt install wget')
 os.system('sudo wget https://github.com/bottlesdevs/Bottles/releases/download/2021.12.28-treviso/com.usebottles.bottles_2021.12.28-treviso_amd64.deb')
 os.system('sudo apt install ./com.usebottles.bottles_2021.12.28-treviso_amd64.deb')
+os.system('sudo rm com.usebottles.bottles_2021.12.28-treviso_amd64.deb')
 print(Fore.BLACK + Back.LIGHTGREEN_EX + 'Thank you for using mojito as your bottle installer' + Style.RESET_ALL)
 print(Fore.BLACK + Back.GREEN + 'bottles should now appear in your menu!' + Style.RESET_ALL)
 print(Fore.GREEN + Back.BLACK + 'To update the bottles, just start the program again' + Style.RESET_ALL)
